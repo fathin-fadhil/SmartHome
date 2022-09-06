c647fb11538aa1bd45a6fa958b9623f631eeb1dc
fathin fadhil <fadhx7@gmail.com>
2022-09-06 17:00:14 +0700

    lightswitch page done and some function optimization

--- js/lamppage.js
+++ js/lamppage.js
@@ -4,9 +4,6 @@ var StatusRelay3;
 var StatusRelay4;
 var StatusRelay5;
 var StatusRelay6;
-var StatusRelay7;
-var StatusRelay8;
-var StatusRelay9;
 
 $(document).ready(function() {
     var database = firebase.database();    
@@ -18,9 +15,6 @@ $(document).ready(function() {
         StatusRelay4 = snap.val().StatusRelay4;
         StatusRelay5 = snap.val().StatusRelay5;
         StatusRelay6 = snap.val().StatusRelay6;
-        StatusRelay7 = snap.val().StatusRelay7;
-        StatusRelay8 = snap.val().StatusRelay8;
-        StatusRelay9 = snap.val().StatusRelay9;
             
         if(StatusRelay1 == "ON"){
             $("#sts-terasdepan").text("ON");
@@ -59,7 +53,7 @@ $(document).ready(function() {
         }
 
         if(StatusRelay4 == "ON"){            
-        $("#sts-ruangtengah").text("ON");
+            $("#sts-ruangtengah").text("ON");
             $("#ic-ruangtengah").addClass("ic-active");
             $("#ic-ruangtengah").removeClass("bi-lightbulb-off-fill");
             $("#ic-ruangtengah").addClass("bi-lightbulb-fill");
@@ -69,50 +63,48 @@ $(document).ready(function() {
             $("#ic-ruangtengah").removeClass("bi-lightbulbfill");
             $("#ic-ruangtengah").addClass("bi-lightbulb-off-fill");
         }
+
+        if(StatusRelay5 == "ON"){            
+            $("#sts-terassamping").text("ON");
+            $("#ic-terassamping").addClass("ic-active");
+            $("#ic-terassamping").removeClass("bi-lightbulb-off-fill");
+            $("#ic-terassamping").addClass("bi-lightbulb-fill");
+        } else {
+            $("#sts-terassamping").text("OFF");
+            $("#ic-terassamping").removeClass("ic-active");
+            $("#ic-terassamping").removeClass("bi-lightbulbfill");
+            $("#ic-terassamping").addClass("bi-lightbulb-off-fill");
+        }
+
+        if(StatusRelay6 == "ON"){            
+            $("#sts-kamarbelakang").text("ON");
+            $("#ic-kamarbelakang").addClass("ic-active");
+            $("#ic-kamarbelakang").removeClass("bi-lightbulb-off-fill");
+            $("#ic-kamarbelakang").addClass("bi-lightbulb-fill");
+        } else {
+            $("#sts-kamarbelakang").text("OFF");
+            $("#ic-kamarbelakang").removeClass("ic-active");
+            $("#ic-kamarbelakang").removeClass("bi-lightbulbfill");
+            $("#ic-kamarbelakang").addClass("bi-lightbulb-off-fill");
+        }
     })
 });
 
+function globalUpdate(field) {
+    var database = firebase.database();
+    var fieldVal;
+    database.ref().child('Relay/' + field).on("value",function(snap){
+        fieldVal = snap.val();
+    })
 
-function togTerasDepan(){
-    var firebaseRef = firebase.database().ref().child("Relay/StatusRelay1");
-        if(StatusRelay1 == "ON"){
+    var firebaseRef = firebase.database().ref().child("Relay/" + field);
+    if(fieldVal == "ON"){
             firebaseRef.set("OFF");
-            StatusRelay1="OFF";
+            fieldVal="OFF";
         } else {
             firebaseRef.set("ON");
-            StatusRelay1="ON";
+            fieldVal="ON";
         }
+    
 }
 
-function togRuangTamu(){
-    var firebaseRef = firebase.database().ref().child("Relay/StatusRelay2");
-        if(StatusRelay2 == "ON"){
-            firebaseRef.set("OFF");
-            StatusRelay2="OFF";
-        } else {
-            firebaseRef.set("ON");
-            StatusRelay2="ON";
-        }
-}
-
-function togKamarDepan(){
-    var firebaseRef = firebase.database().ref().child("Relay/StatusRelay3");
-        if(StatusRelay3 == "ON"){
-            firebaseRef.set("OFF");
-            StatusRelay3="OFF";
-        } else {
-            firebaseRef.set("ON");
-            StatusRelay3="ON";
-        }
-}
-
-function togRuangTengah(){
-    var firebaseRef = firebase.database().ref().child("Relay/StatusRelay4");
-        if(StatusRelay4 == "ON"){
-            firebaseRef.set("OFF");
-            StatusRelay4="OFF";
-        } else {
-            firebaseRef.set("ON");
-            StatusRelay4="ON";
-        }
-}
